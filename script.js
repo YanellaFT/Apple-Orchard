e1d449a30d92abf59899ff70c0429ce47365d4f4
YanellaFT <yanellaft@gmail.com>
2025-07-23 21:45:11 +0000

    added playButton

--- script.js
+++ script.js
@@ -7,12 +7,18 @@ function preload(){
 
 /* SETUP RUNS ONCE */
 function setup() {
-  createCanvas(400,400);
+  createCanvas(500,500);
+
+  playButton = new Sprite(-200, -200, 100, 100);
 
 }
 
 /* DRAW LOOP REPEATS */
 function draw() {
+  //start screen
+  background() //need to draw!!!
+  playButton.pos = {x: 250, y: 400};
+  
 
 }
 
