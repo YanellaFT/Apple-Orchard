08d5d4ae2e373e0b5c99e5fcd7f1d1b51d1c1936
YanellaFT <yanellaft@gmail.com>
2025-07-26 18:39:12 +0000

    added basket + movement

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /* VARIABLES */
 let screen = 0;
 let fallingApple;
+let basket;
+let score = 0;
+let next1;
 
 /* PRELOAD LOADS FILES */
 function preload(){
@@ -21,6 +24,10 @@ function setup() {
   fallingApple = new Sprite(-200,-200);
   fallingApple.diameter = 20;
 
+  basket = new Sprite(-200,-200, 100, 45, "k");
+
+  next1 = new Sprite(-200, -200, 60, 30);
+
   
   //SCREEN 0
   background("#86cbd9"); //DRAWWWW!!!!
@@ -40,7 +47,6 @@ function setup() {
 
 }
 
-/* DRAW LOOP REPEATS */
 function draw() {
   
   if (screen == 0) {
@@ -73,21 +79,63 @@ function draw() {
   if (screen == 1) {
     background("pink"); //DRAWWWW!!!!
     textSize(15);
-    text("Step 1: Collect all the apples \nby using the arrow keys", 200, 35);
+    text("Step 1: Collect all the apples \nby using the arrow keys", 120, 35);
+
+    textSize(13);
+    text("Apples Collected: " + score, 330, 35);    
     
+    //falling apples
     if (fallingApple.y >= height) {
       fallingApple.y = 0;
       fallingApple.x = random(width);
-      fallingApple.vel.y = random(1,5);
+      fallingApple.vel.y = random(3,6);
     }
-  }
 
-}
+    //moving basket
+    if (kb.pressing("left")) {
+      basket.vel.x = -4;
+    } else if (kb.pressing("right")) {
+      basket.vel.x = 4;
+    } else {
+      basket.vel.x = 0;
+    }
+
+    //stop basket at edges
+    if (basket.x < 45) {
+      basket.vel = 0;
+    } else if (basket.x > 355) {
+      basket.vel = 0;
+    }
+
+    //apples collides with basket
+    if (fallingApple.collides(basket)) {
+      fallingApple.y = 0;
+      fallingApple.x = random(width);
+      fallingApple.vel.y = random(3,6);
+      fallingApple.direction = "down";
+      score = score + 1;
+    }
+
+    //continue button at score == 15
+    //FIX THIS
+    if (score == 4) {
+      textSize(10);
+      text("Press to \ncontinue", 380, 360);
+
+      next1.pos = {x: 380, y: 380};
+    }
+    
+  } //end of screen == 1
+
+} //end of draw funciton
 
 /* FUNCTIONS */
 function showScreen1() {
   startButton.pos = {x: -100, y: -100};
+  
   fallingApple.x = 50;
   fallingApple.y = 0;
-  fallingApple.vel.y = 2;
+  fallingApple.vel.y = 3;
+
+  basket.pos = {x: 200, y: 360};
 }
